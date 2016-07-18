d9f0786dcc7545fa6a9c709ee6ec1d221d9aadc7
Max Azarcon <contact@maxazarcon.com>
2016-07-18 08:42:56 -0400

    push

--- order.js
+++ order.js
@@ -16,7 +16,7 @@ $(function () {
     var option = [130], // Sets number of options
         select = $("#options"),
         i = 0;
-    for (i = 0; i < 130; i++) {
+    for (i = 0; i < 130; i = i + 1) {
         option[i] = document.createElement("OPTION");
         option[i].value = i + 1;
         option[i].text = i + 1;
@@ -26,18 +26,40 @@ $(function () {
     /**
      * Get totals from sections
      */
+    
+    function package_totals() {
+        return 0;
+        
+        //return packagetotal;
+    }
+    
+    function img_totals() {
+        var imgtotal = $('input[name="imgselect"]:checked').length * 5.25;
+        
+        return imgtotal;
+    }
+    
+    function alacarte_totals() {
+        return 0;
+        
+        //return alacartetotal
+    }
+    
+    function get_totals() {
+        var total = package_totals() + img_totals() + alacarte_totals();
+        $('#totalright').text("$" + total.toFixed(2));
+    }
+    
     $('input[name="imgselect"]').click(function () {
-        
-        var imgtotal = $('input[name="imgselect"]:checked').length * 5.25,
-            imgtotalfixed = imgtotal.toFixed(2);
-        
+        get_totals();
         if ($('#imgtotal').length === 0) {
             $('#ordertotal').before("<div class='total' id='imgtotal''>Selected Images (x" + $('input[name="imgselect"]:checked').length + ")</div>");
         } else {
             $('#imgtotal').text("Selected Images (x" + $('input[name="imgselect"]:checked').length + ")");
         }
         
-        
-        $('#totalright').text("$" + imgtotalfixed);
+        if ($('input[name="imgselect"]:checked').length === 0) {
+            $('#imgtotal').remove();
+        }
     });
 });
