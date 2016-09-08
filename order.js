18b188f1640eb28c1056c8f227c0e97c8fe77de6
Max Azarcon <contact@maxazarcon.com>
2016-09-08 15:12:46 -0400

    Update 8 Sept 2016

--- order.js
+++ order.js
@@ -1,172 +1,228 @@
 $(function () {
-    "use strict";
-    
-    var imgSelector = [];
-    $('input[name="imgselect"]').click(function () {
-        imgSelector.push($(this).attr('value'));
-    });
-    
-     // Toggles active class on packages
-    
+	"use strict";
+	
+	var imgSelector = [];
+	$('input[name="imgselect"]').click(function () {
+		imgSelector.push($(this).val());
+	});
+	
+	// Toggles active class on packages
+	
 	$('.collection input[type="checkbox"]').click(function () {
-        if ($(this).parents('.collection').hasClass('active')) {
-            $(this).parents('.collection').removeClass('active');
-            $('.step#selection').slideUp();
-            $('.step#alacarte').slideDown();
-        } else {
-            $(this).parents('.collection').addClass('active');
-            $(this).parents('.collection').siblings('.collection').removeClass('active');
-            $(this).parents('.collection').siblings('.collection').find('.collectionselector').prop('checked', false);
-            $('#openalc').show();
-            $('.step#selection').slideDown();
-            $('.step#alacarte').slideUp();
-            imgSelector = [];
-        }
-    });
-    
-    $('#openalc').click(function () {
-        $('.step#alacarte').slideDown();
-        $(this).hide();
-    });
-    
-    /**
-     * Set maximum number of images/package
-     */
-    
-    var select_max,
-        max = 130, // Sets number of options
-        option = [max],
-        select = $(".options"),
-        i = 0;
-    
-    function set_max() {
-        
-        if ($('.collection#heirloom').hasClass('active')) {
-            select_max = 50;
-        } else if ($('.collection#signature').hasClass('active')) {
-            select_max = 36;
-        } else if ($('.collection#keepsake').hasClass('active')) {
-            select_max = 24;
-        } else {
-            select_max = 0;
-        }
-        
-        $('input[name="imgselect"]').on('change', function (evt) {
-            if ($('input[name="imgselect"]:checked').length > select_max) {
-                this.checked = false;
-                $('#selected').text(select_max);
-            }
-        });
-        
-        $('#selected').text($('input[name="imgselect"]:checked').length);
-        $('.selectmax').text(select_max);
-        
-        return select_max;
-    }
-    
-    // Get images from selector
-    
-    $('.collection').click(function () {
-        $('input[name="imgselect"]:checked').prop('checked', false);
-    });
-    
-    /**
-     * Set number of options in image dropdown
-     */
+		if ($(this).parents('.collection').hasClass('active')) {
+			$(this).parents('.collection').removeClass('active');
+			$('.step#selection').slideUp();
+			$('.step#alacarte').slideDown();
+		} else {
+			$(this).parents('.collection').addClass('active');
+			$(this).parents('.collection').siblings('.collection').removeClass('active');
+			$(this).parents('.collection').siblings('.collection').find('.collectionselector').prop('checked', false);
+			$('#openalc').show();
+			$('.step#selection').slideDown();
+			$('.step#alacarte').slideUp();
+			imgSelector = [];
+		}
+	});
+	
+	$('#openalc').click(function () {
+		$('.step#alacarte').slideDown();
+		$(this).hide();
+	});
+	
+	var select_max,
+		max = 130, // Sets number of options
+		option = [max],
+		select = $(".options"),
+		i = 0;
+	
+	function set_max() {
+		
+		if ($('.collection#heirloom').hasClass('active')) {
+			select_max = 50;
+		} else if ($('.collection#signature').hasClass('active')) {
+			select_max = 36;
+		} else if ($('.collection#keepsake').hasClass('active')) {
+			select_max = 24;
+		} else {
+			select_max = 0;
+		}
+		
+		$('input[name="imgselect"]').on('change', function (evt) {
+			if ($('input[name="imgselect"]:checked').length > select_max) {
+				this.checked = false;
+				$('#selected').text(select_max);
+			}
+		});
+		
+		$('#selected').text($('input[name="imgselect"]:checked').length);
+		$('.selectmax').text(select_max);
+		
+		return select_max;
+	}
+	
+	// Get images from selector
+	
+	$('.collection').click(function () {
+		$('input[name="imgselect"]:checked').prop('checked', false);
+	});
+	
+	// Set number of options in image dropdown
 
-    for (i = 0; i < max; i = i + 1) {
-        option[i] = document.createElement("OPTION");
-        option[i].value = i + 1;
-        option[i].text = i + 1;
-        select.append(option[i]);
-    }
-    
-    
-    /**
-     * Add rows to A La Carte selection
-     */
-    
-    $('#alcmore').click(function () {
-        $(this).before("<tr class='alcselection'><td><input class='alcimg' type='text'></td><td><select class='alcsize'><option selected disabled>Select a Size</option><option disabled>---</option><option value='4x6 Print'>4x6 Print</option><option value='5x7 Print'>5x7 Print</option><option value='8x10 Print'>8x10 Print</option><option value='8x12 Panoramic'>8x12 Panoramic</option><option value='Single Digital Image File'>Single Digital Image File</option><option value='Set of 3 Digital Image Files'>Set of 3 Digital Image Files</option></select></td><td><input class='alcqty'></td><td><span class='alcprice'></span></td><td><span class='alctotal'></span></td><td class='removealc'><i class='fa fa-minus-circle' style='color: red'></i></td></tr>");
-    });
-    
-    /**
-     * Remove rows from A La Carte selection
-     */
-    
-    $('table').on('click', '.removealc', function (events) {
-        $(this).parents('tr').remove();
-    });
-    
-    // Add A La Carte totals / row
-    
-    /**
-     * Get totals from sections
-     */
-    
-    function package_totals() {
-        
-        if ($('.collection#heirloom').hasClass('active')) {
-            return 1245;
-        } else if ($('.collection#signature').hasClass('active')) {
-            return 955;
-        } else if ($('.collection#keepsake').hasClass('active')) {
-            return 695;
-        } else if ($('.collection#gift').hasClass('active')) {
-            return 300;
-        } else {
-            return 0;
-        }
-    }
-    
-    function alacarte_totals() {
-        return 0;
-        
-        //return alacartetotal
-    }
-    
-    function order_list() {
-        if ($('.collection#heirloom').hasClass('active')) {
-            $('.totalitem.package').html(
-                '<p class="left">Heirloom Collection</span><span class="right">$1245.00</p>' +
-                    '<p style="display: none;">Images Selected: ' + imgSelector.sort(function (a, b) {return a - b; }).toString() + '</p>'
-            );
-        } else if ($('.collection#signature').hasClass('active')) {
-            $('.totalitem.package').html(
-                'Signature Collection<span class="right">$955.00</span>' +
-                    '<p style="display: none;">Images Selected: ' + imgSelector.sort(function (a, b) {return a - b; }).toString() + '</p>'
-            );
-        } else if ($('.collection#keepsake').hasClass('active')) {
-            $('.totalitem.package').html(
-                'Keepsake Collection<span class="right">$695.00</span>' +
-                    '<p style="display: none;">Images Selected: ' + imgSelector.sort(function (a, b) {return a - b; }).toString() + '</p>'
-            );
-        } else {
-            $('.totalitem.package').html('');
-        }
-    }
-    
-    function get_totals() {
-        var subtotal = package_totals() + alacarte_totals(),
-            tax = subtotal * 0.0675,
-            total = subtotal + tax;
-        $('#taxright').text("$" + tax.toFixed(2));
-        $('#totalright').text("$" + total.toFixed(2));
-    }
-    
-    $(window).click(function () {
-        set_max();
-        order_list();
-        get_totals();
-    });
-    
-    var orderForm = $('#ordertotalmain');
-    
-    $('#orderSubmit').click(function () {
-        $.ajax({
-            type: 'POST',
-            url: 'sendmail.php',
-            data: { content: orderForm.html() }
-        });
-    });
+	for (i = 0; i < max; i = i + 1) {
+		option[i] = document.createElement("OPTION");
+		option[i].value = i + 1;
+		option[i].text = i + 1;
+		select.append(option[i]);
+	}
+	
+	// Add rows to A La Carte selection
+	
+	$('#alcmore').click(function () {
+		$(this).before("<tr class='alcselection'><td><input class='alcimg' type='text'></td><td><select class='alcsize'><option selected disabled>Select a Size</option><option disabled>---</option><option value='4x6 Print'>4x6 Print</option><option value='5x7 Print'>5x7 Print</option><option value='8x10 Print'>8x10 Print</option><option value='8x12 Panoramic'>8x12 Panoramic</option><option value='Single Digital Image File'>Single Digital Image File</option><option value='Set of 3 Digital Image Files'>Set of 3 Digital Image Files</option></select></td><td><input class='alcqty'></td><td><span class='alcprice'></span></td><td><span class='alctotal'></span></td><td class='removealc'><i class='fa fa-minus-circle' style='color: red'></i></td></tr>");
+	});
+	
+	// Remove rows from A La Carte selection
+	
+	$('table').on('click', '.removealc', function (events) {
+		$(this).parents('tr').remove();
+	});
+	/*
+	 * Add A La Carte totals / row
+	 */
+
+	// Update on size change
+
+	$('.alcsize').on('change', function(){
+		if($(this).val() === '4x6 Print') {
+			$(this).parents('td').siblings().find('.alcprice').html('<span value="35">$35</span>');
+		} else if($(this).val() === '5x7 Print') {
+			$(this).parents('td').siblings().find('.alcprice').html('<span value="35">$35</span>');
+		} else if($(this).val() === '8x10 Print') {
+			$(this).parents('td').siblings().find('.alcprice').html('<span value="35">$35</span>');
+		} else if($(this).val() === '8x12 Panoramic') {
+			$(this).parents('td').siblings().find('.alcprice').html('<span value="40">$40</span>');
+		} else if($(this).val() === 'Single Digital Image File') {
+			$(this).parents('td').siblings().find('.alcprice').html('<span value="120">$120</span>');
+		} else if($(this).val() === 'Set of 3 Digital Image Files') {
+			$(this).parents('td').siblings().find('.alcprice').html('<span value="300">$300</span>');
+		} else {
+			return false;
+		}
+
+		$(this).parents('td').siblings().find('.alctotal').html('<span>$' + $(this).parents('td').siblings().find('.alcqty').val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value') + '</span>' );
+	});
+
+	// Update on quantity change
+
+	$('.alcqty').on('change', function() {
+		$(this).parents('td').siblings().find('.alctotal').html('<span>$' + $(this).val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value') + '</span>' );
+	});
+
+	/**
+	 * Get totals from sections
+	 */
+	
+	function package_totals() {
+		
+		if ($('.collection#heirloom').hasClass('active')) {
+			return 1245;
+		} else if ($('.collection#signature').hasClass('active')) {
+			return 955;
+		} else if ($('.collection#keepsake').hasClass('active')) {
+			return 695;
+		} else if ($('.collection#gift').hasClass('active')) {
+			return 300;
+		} else {
+			return 0;
+		}
+	}
+	
+	function alacarte_totals() {
+		return 0;
+		
+		//return alacartetotal
+	}
+
+	function folioItems() {
+		$('.collection.active input[type="text"').each(function(){
+			return $(this).val();
+		});
+	}
+	
+	function order_list() {
+		if ($('.collection#heirloom').hasClass('active')) {
+			$('.totalitem.package').html(
+				'<tr>\n' +
+				'<td class="left">Heirloom Collection</td>\n' +
+				'<td class="right">$1245.00</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">Folio Images Selected</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">' + $('.collection.active input[type="text"]').val() + '</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">Album Images Selected:</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">' + imgSelector.sort(function (a, b) {return a - b; }).join(', ') + '</td>\n' +
+				'</tr>'
+			);
+		} else if ($('.collection#signature').hasClass('active')) {
+			$('.totalitem.package').html(
+				'<tr>\n' +
+				'<td class="left">Signature Collection</td>\n' +
+				'<td class="right">$955.00</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">Images Selected:</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">' + imgSelector.sort(function (a, b) {return a - b; }).join(', ') + '</td>\n' +
+				'</tr>'
+			);
+		} else if ($('.collection#keepsake').hasClass('active')) {
+			$('.totalitem.package').html(
+				'<tr>\n' +
+				'<td class="left">Keepsake Collection</td>\n' +
+				'<td class="right">$695.00</td>\n' +
+				'</tr>\n' +
+				'<tr>\n' +
+				'<tr class="form-hidden">' +
+				'<td colspan="2">Images Selected:</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">' + imgSelector.sort(function (a, b) {return a - b; }).join(', ') + '</td>\n' +
+				'</tr>'
+			);
+		} else {
+			$('.totalitem.package').html('');
+		}
+	}
+	
+	function get_totals() {
+		var subtotal = package_totals() + alacarte_totals(),
+			tax = subtotal * 0.0675,
+			total = subtotal + tax;
+		$('#taxright').text("$" + tax.toFixed(2));
+		$('#totalright').text("$" + total.toFixed(2));
+	}
+	
+	$(window).click(function () {
+		set_max();
+		order_list();
+		get_totals();
+		folioItems();
+	});
+	
+	var orderForm = $('#ordertotalmain');
+	
+	$('#orderSubmit').click(function () {
+		$.ajax({
+			type: 'POST',
+			url: 'sendmail.php',
+			data: { content: orderForm.html() }
+		});
+	});
 });
--- sendmail.php
+++ sendmail.php
@@ -6,16 +6,14 @@ $subject = 'Photo Order Form';
 $form = $_POST['content'];
 
 $message = '<html>';
-$message .= '<head>';
-$message .= '<link href="//sevenagesdesign.com/clients/wwp/style.css" rel="stylesheet" type="text/css"></link>';
-$message .= '</head>';
 $message .= '<body>';
 $message .= $form;
 $message .= '</body></html>';
 
-$headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'From: max@sevenagesdesign.com' . "\r\n";
-$headers .= 'Reply-To: contact@maxazarcon.com' . "\r\n";
+$headers = "MIME-Version: 1.0\r\n";
+$headers .= "From: max@sevenagesdesign.com\r\n";
+$headers .= "Reply-To: contact@maxazarcon.com\r\n";
+# $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 $headers .= 'X-Mailer: PHP/' . phpversion();
 
 mail($to, $subject, strip_tags($message), $headers);
