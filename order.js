5c3abe03aa4c60e64544bcb7d228b22084ece895
Max Azarcon <contact@maxazarcon.com>
2016-09-15 16:24:56 -0400

    Almost done!
    yaaay

--- order.js
+++ order.js
@@ -1,7 +1,9 @@
-$(function () {
+$(document).ready(function () {
 	"use strict";
 
 	var totalAvailable = 130; // Set total number of album images available
+	var setCouponCode = "couponcode200"; // Sets the coupon code
+	var setCouponDiscount = 200; // Sets amount coupon takes off order
 
 
 	// Firefox bug workaround
@@ -24,6 +26,7 @@ $(function () {
 			var index = imgSelector.indexOf($(this).val());
 			imgSelector.splice(index, 1);  
 		}
+		order_list();
 	});
 
 	// Create Photo Select Checkboxes
@@ -34,16 +37,8 @@ $(function () {
 		}
 	});
 
-	// Prepare coupon code
-
-	$('#couponcode').css('width', 'calc(100% - ' + $('#addcoupon').outerWidth() * 1.75 + 'px)');
-
-	
-
-
 	// Toggles active class on packages
-	
-	$('.collection').on('click', 'input[type="checkbox"]', function () {
+	$('.collection').on('change', 'input[type="checkbox"]', function () {
 		if ($(this).parents('.collection').hasClass('active')) {
 			$(this).parents('.collection').removeClass('active');
 			$('.step#selection').slideUp();
@@ -57,7 +52,74 @@ $(function () {
 			$('.step#alacarte').slideUp();
 			imgSelector = [];
 		}
+		updatePrices();
 	});
+
+	$('.collection input[type="checkbox"]').click(function(){
+		clearSelected();
+	});
+
+	function clearSelected() {
+		$('#selected').text('0');
+	}
+
+	var price_4x6,
+		price_5x7,
+		price_8x10,
+		price_8x12,
+		price_sdif,
+		price_3dif;
+
+	function updatePrices(){
+		if ($('.collection').hasClass('active')) {
+		// Set prices for A La Carte if Collection is selected
+
+			price_4x6 =		20;	// Price for 4x6 Print
+			price_5x7 =		20; // Price for 5x7 Print
+			price_8x10 =	30; // Price for 8x10 Print
+			price_8x12 = 	35; // Price for 8x12 Panoramic
+			price_sdif =	80; // Price for Single Digital Image File
+			price_3dif =	200; // Price for Set of 3 Digital Image Files
+		} else {
+		// Set prices for A La Carte if NO Collection is selected
+
+			price_4x6 =		35;	// Price for 4x6 Print
+			price_5x7 =		35; // Price for 5x7 Print
+			price_8x10 =	35; // Price for 8x10 Print
+			price_8x12 = 	40; // Price for 8x12 Panoramic
+			price_sdif =	120; // Price for Single Digital Image File
+			price_3dif =	300; // Price for Set of 3 Digital Image Files
+		}
+		printPrices();
+
+	}
+
+	function printPrices(){
+
+		$('.alcsize').each(function(){
+
+			if($(this).val() === '4x6 Print') {
+				$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_4x6 + '">$' + price_4x6 +'</span>');
+			} else if($(this).val() === '5x7 Print') {
+				$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_5x7 + '">$' + price_5x7 + '</span>');
+			} else if($(this).val() === '8x10 Print') {
+				$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_8x10 + '">$' + price_8x10 + '</span>');
+			} else if($(this).val() === '8x12 Panoramic') {
+				$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_8x12 + '">$' + price_8x12 + '</span>');
+			} else if($(this).val() === 'Single Digital Image File') {
+				$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_sdif + '">$' + price_sdif + '</span>');
+			} else if($(this).val() === 'Set of 3 Digital Image Files') {
+				$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_3dif + '">$' + price_3dif +'</span>');
+			} else {
+				return false;
+			}
+
+			$(this).parents('td').siblings().find('.alctotal').html('<span>$' + $(this).parents('td').siblings().find('.alcqty').val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value') + '</span>' );
+			$(this).parents('td').siblings().find('.alctotal span').val($(this).parents('td').siblings().find('.alcqty').val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value'));
+		});
+
+		alcSelections();
+	}
 	
 	$('#openalc').click(function () {
 		$('.step#alacarte').slideDown();
@@ -78,6 +140,8 @@ $(function () {
 			select_max = 36;
 		} else if ($('.collection#keepsake').hasClass('active')) {
 			select_max = 24;
+		} else if ($('.collection#gift').hasClass('active')) {
+			select_max = 12;
 		} else {
 			select_max = 0;
 		}
@@ -136,8 +200,10 @@ $(function () {
 	
 	// Remove rows from A La Carte selection
 	
-	$('table').on('click', '.removealc', function (events) {
+	$('table').on('click', '.removealc', function () {
+		$(this).parents('tr').find('.alcqty').val(0);
 		$(this).parents('tr').remove();
+		alcSelections();
 	});
 
 	/*
@@ -147,51 +213,17 @@ $(function () {
 	// Update on size change
 
 	$('table').on('change', '.alcsize', function(events){
-		if ($('.collection').hasClass('active')) {
-		// Set prices for A La Carte if Collection is selected
-
-		var price_4x6 =		20,	// Price for 4x6 Print
-			price_5x7 =		20, // Price for 5x7 Print
-			price_8x10 =	30, // Price for 8x10 Print
-			price_8x12 = 	35, // Price for 8x12 Panoramic
-			price_sdif =	80, // Price for Single Digital Image File
-			price_3dif =	200 // Price for Set of 3 Digital Image Files
-		} else {
-		// Set prices for A La Carte if NO Collection is selected
-
-		var price_4x6 =		35,	// Price for 4x6 Print
-			price_5x7 =		35, // Price for 5x7 Print
-			price_8x10 =	35, // Price for 8x10 Print
-			price_8x12 = 	40, // Price for 8x12 Panoramic
-			price_sdif =	120, // Price for Single Digital Image File
-			price_3dif =	300 // Price for Set of 3 Digital Image Files
-		}
-
-		if($(this).val() === '4x6 Print') {
-			$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_4x6 + '">$' + price_4x6 +'</span>');
-		} else if($(this).val() === '5x7 Print') {
-			$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_5x7 + '">$' + price_5x7 + '</span>');
-		} else if($(this).val() === '8x10 Print') {
-			$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_8x10 + '">$' + price_8x10 + '</span>');
-		} else if($(this).val() === '8x12 Panoramic') {
-			$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_8x12 + '">$' + price_8x12 + '</span>');
-		} else if($(this).val() === 'Single Digital Image File') {
-			$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_sdif + '">$' + price_sdif + '</span>');
-		} else if($(this).val() === 'Set of 3 Digital Image Files') {
-			$(this).parents('td').siblings().find('.alcprice').html('<span value="' + price_3dif + '">$' + price_3dif +'</span>');
-		} else {
-			return false;
-		}
-
-		$(this).parents('td').siblings().find('.alctotal').html('<span>$' + $(this).parents('td').siblings().find('.alcqty').val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value') + '</span>' );
-		$(this).parents('td').siblings().find('.alctotal span').val($(this).parents('td').siblings().find('.alcqty').val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value'));
+		updatePrices();
 	});
 
 	// Update on quantity change
 
-	$('table').on('change', '.alcqty', function(events) {
-		$(this).parents('td').siblings().find('.alctotal').html('<span>$' + $(this).val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value') + '</span>' );
-		$(this).parents('td').siblings().find('.alctotal span').val($(this).val() * $(this).parents('td').siblings().find('.alcprice > span').attr('value'));
+	$('table').on('change', '.alcqty', function() {
+		updatePrices();
+	});
+
+	$('table').on('change', '.alcimg', function() {
+		updatePrices();
 	});
 
 	// Get totals from sections
@@ -218,22 +250,6 @@ $(function () {
 			alctotalsfinal += parseInt($(this).val());
 			return alctotalsfinal;
 		});
-		if (alctotalsfinal > 0) {
-			$('.totalitem.alctotals').html(
-				'<tr>\n' +
-				'<td class="left">A La Carte Items</td>\n' +
-				'<td class="right">$' + alctotalsfinal + '</td>\n' +
-				'</tr>\n' +
-				'<tr>\n' +
-				'<td colspan="2">A La Carte Selections:</td>\n' +
-				'</tr>\n' +
-				'<tr>\n' +
-				'<td>Placeholder Text</td>\n' +
-				'</tr>'
-			);
-		} else {
-			$('.totalitem.alctotals').html('');
-		}
 		return alctotalsfinal;
 		//return alacartetotal
 	}
@@ -306,6 +322,20 @@ $(function () {
 				'<td colspan="2">' + imgSelector.sort(function (a, b) {return a - b; }).join(', ') + '</td>\n' +
 				'</tr>'
 			);
+		} else if ($('.collection#gift').hasClass('active')) {
+			$('.totalitem.package').html(
+				'<tr>\n' +
+				'<td class="left">Gift Print Collection</td>\n' +
+				'<td class="right">$300.00</td>\n' +
+				'</tr>\n' +
+				'<tr>\n' +
+				'<tr class="form-hidden">' +
+				'<td colspan="2">Images Selected:</td>\n' +
+				'</tr>\n' +
+				'<tr class="form-hidden">\n' +
+				'<td colspan="2">' + imgSelector.sort(function (a, b) {return a - b; }).join(', ') + '</td>\n' +
+				'</tr>'
+			);
 		} else {
 			$('.totalitem.package').html('');
 		}
@@ -315,13 +345,14 @@ $(function () {
 
 	$('#addcoupon').click(function(e){
 		e.preventDefault();
-		if ($('#couponcode').val() == "couponcode200") {
-			coupon = 200;
+		if ($('#couponcode').val() == setCouponCode && $('.collection').hasClass('active')) {
+			coupon = setCouponDiscount;
 			get_totals();
 			$('#couponcode').val('Coupon successfully redeemed!');
 			$('#couponcode').css({'background-color': '#5cb85c', 'width': '100%', 'text-align': 'center', 'border': '0', 'line-height': '26px', 'color': 'white', 'margin': '12px'});
 			$('#couponcode').attr('disabled', 'disabled');
-			$('#ordertotalinner').before('<tr style="width: 50%; margin: auto;"><td>Coupon</td><td style="float: right;">- $200.00</td></tr>');
+			$('#ordertotalinner').before('<tr style="width: 50%; margin: auto;"><td>Coupon</td><td id="coupondiscount" style="float: right;">' +
+				'- $200.00</td></tr>');
 			$('#addcoupon').remove();
 			setTimeout(function(){
 				$('#couponcode').fadeOut(1000);
@@ -331,7 +362,11 @@ $(function () {
 			}, 2000);
 		} else {
 			$('#couponcode').css({'border-color': '#d9534f'});
-			$('#couponcode').attr('placeholder', 'Invalid coupon code.');
+			if ($('#couponcode').val != setCouponCode && $('.collection').hasClass('active')){
+				$('#couponcode').attr('placeholder', 'Invalid coupon code.');
+			} else {
+				$('#couponcode').attr('placeholder', 'Coupon not valid for this order.');
+			}
 			$('#couponcode').val(null);
 			setTimeout(function(){
 				$('#couponcode').css({'border-color': 'initial'});
@@ -341,11 +376,51 @@ $(function () {
 	});
 	
 	function get_totals() {
-		var subtotal = package_totals() + alacarte_totals(),
+		var alacarteSubtotal = alacarte_totals(),
+			alacarteDiscount = .40 * alacarteSubtotal,
+			alacarteTotal = ($('.collection').hasClass('active') ? alacarteSubtotal - alacarteDiscount : alacarteSubtotal),
+			subtotal = package_totals() + alacarteTotal,
 			tax = subtotal * 0.0675,
 			total = subtotal + tax - coupon;
 		$('#taxright').text("$" + tax.toFixed(2));
 		$('#totalright').text("$" + total.toFixed(2));
+	}
+
+	function alcSelections() {
+
+		var alctotalsfinal = alacarte_totals();
+		if ($('.alcselection').length > 0) {
+			$('.totalitem.alctotals').html(
+				'<tr>\n' +
+				'<td class="left">A La Carte Items</td>\n' +
+				'<td class="right">$' + alctotalsfinal + '</td>\n' +
+				'</tr>\n' +
+				'<tr id="alcdata">\n' +
+				'<td colspan="2">A La Carte Selections:</td>\n' +
+				'</tr>\n' +
+				'<tr class="alcdatatotal">\n' +
+				'</tr>'
+			);
+		} else {
+			$('.totalitem.alctotals').html('');
+		}
+
+
+		$('.alcselection').each(function(){
+			var imagenum = $(this).find('.alcimg').val().toString(),
+		    	imagesize = $(this).find('.alcsize').val().toString(),
+		    	imgqty = ($(this).find('.alcqty').val().toString() == "" ? "0" : $(this).find('.alcqty').val().toString()),
+		    	fullcost = $(this).find('.alctotal span').val().toString();
+
+			$('.totalitem.alctotals').append(
+				'<tr class="alcdatatotal">' +
+				'<td style="float:left;">#' + imagenum + ' - ' + imagesize + ' (x' + imgqty + ')</td>' +
+				'<td style="float:right;text-align:right;">$' + fullcost + '</td>' +
+				'</tr>' +
+				'\r\n'
+			);
+
+		});
 	}
 	
 	$(window).change(function () {
@@ -353,14 +428,88 @@ $(function () {
 		order_list();
 		get_totals();
 	});
+
+	function sendEmail() {
+		var collection = $('.totalitem.package'),
+			alcpackage = $('.totalitem.alctotals'),
+			dbt = $('input[name="debutante"]'),
+			dbtNum = $('input[name="debNumber"]'),
+			fName = $('input[name="firstName"]'),
+			lName = $('input[name="lastName"]'),
+			addr = $('input[name="streetAddress"]'),
+			city = $('input[name="city"]'),
+			state = $('input[name="state"]'),
+			zip = $('input[name="zipcode"]'),
+			p1 = $('input[name="phone1"]'),
+			p2 = $('input[name="phone2"]'),
+			p3 = $('input[name="phone3"]'),
+			email = $('input[name="email"]'),
+			totaltax = $('#taxright'),
+			couponused = $('#coupondiscount'),
+			totalcost = $('#totalright');
+
+		$.ajax(
+			{
+				type: 'POST',
+				url: 'sendmail.php',
+				data: {
+					collection: collection.html(),
+					alc: alcpackage.html(),
+					dbt: dbt.val(),
+					dbtNum: dbtNum.val(),
+					fName: fName.val(),
+					lName: lName.val(),
+					addr: addr.val(),
+					city: city.val(),
+					state: state.val(),
+					zip: zip.val(),
+					p1: p1.val(),
+					p2: p2.val(),
+					p3: p3.val(),
+					email: email.val(),
+					ship: $('input[name="shippickup"]').attr('data'),
+					pay: $('input[name="payment"]:checked').attr('data'),
+					tax: totaltax.text(),
+					total: totalcost.text(),
+					coupon: couponused.text()
+				}
+			}
+		);
+	}
 	
-	var orderForm = $('#ordertotalmain');
-	
-	$('#orderSubmit > button').click(function () {
-		$.ajax({
-			type: 'POST',
-			url: 'sendmail.php',
-			data: { content: orderForm.html() }
+	function validateCollection() {
+		var success = true;
+
+		$('.collection.active input[type="text"').each(function(){
+			if($(this).val() == "") {
+				$(this).first().focus();
+				success = false;
+				return success;
+			}
 		});
+
+		return success;
+	}
+
+	function validateContact() {
+		var success = true;
+
+		$('#contactinfo input[type="text"').each(function(){
+			if($(this).val() == "") {
+				$(this).first().focus();
+				success = false;
+			}
+		});
+
+		return success;
+	}
+
+	$('#orderSubmit > button').click(function () {
+		
+		if (validateCollection() == false) {
+			return false;
+		} else {
+			return false;
+		}
 	});
 });
